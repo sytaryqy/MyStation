ba9d014efeabc9174de129c6ff15112c818eab39
sytar <sytar@sytartekimac.local>
2015-12-15 16:38:13 +0800

    My first game project

---
+++ Higher Or Lower/Higher Or Lower/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Higher Or Lower
+//
+//  Created by sytar on 15/12/15.
+//  Copyright © 2015年 sytar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int answer=0;
+        int guess=0;
+        int turn=0;
+        
+    }
+    return 0;
+}
