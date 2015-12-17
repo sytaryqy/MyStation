cdf97a0422a0d024ee515976ae898d3d6bb41fa9
sytar <sytar@sytartekimac.local>
2015-12-17 16:13:18 +0800

    String object -using the NSString

--- PeopleDatabase/PeopleDatabase/main.m
+++ PeopleDatabase/PeopleDatabase/main.m
@@ -11,7 +11,23 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSString *helloWorld=@"Hello, World!";
+        NSLog(@"%@",helloWorld);
+        
+        //use nsstring object show the string.
+        int i=10;
+        NSString *stringShow=[NSString stringWithFormat:@"The number is %i",i];
+        NSLog(@"%@",stringShow);
+        
+        //scanf can only used in int/char/float/bool
+        
+        //How to use scanf load the words.
+        NSLog(@"Please input a word.");
+        char cstring[40];
+        scanf("%s",cstring);
+        NSString  *stringInput=[NSString stringWithCString:cstring encoding:1];
+        NSLog(@"%@",stringInput);
     }
     return 0;
 }
