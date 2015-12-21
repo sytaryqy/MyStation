d2204fba759c733c04eeae90d3374b1075b59553
sytar <sytar@sytartekimac.local>
2015-12-21 16:21:00 +0800

    Tap me! Edited ViewController.

--- Tap Me/Tap Me/ViewController.h
+++ Tap Me/Tap Me/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *scoreLabel;
+    IBOutlet UILabel *timerLabel;
+}
 
+-(IBAction)buttonPressed;
 
 @end
 
