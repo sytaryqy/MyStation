1a3e524deb79cd574e4b957219b8f1a075c7824d
sytar <sytar@sytartekimac.local>
2016-01-05 16:48:54 +0800

    BullsEye--Added the startNewRound method.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentSliderValue:Int=0;
+    @IBOutlet weak var slider: UISlider!
+    var targetValue:Int=0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //currentSliderValue=lroundf(slider.value)
+        //targetValue=1+Int(arc4random_uniform(100))
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +27,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert(){
-        let alert=UIAlertController (title: "Hello,world!", message: "This is my first app!", preferredStyle: .Alert)
-        let action=UIAlertAction (title: "Awesome", style: .Default, handler: nil)
+        let message="The value of the slider is: \(currentSliderValue)"+"\nThe target value is: \(targetValue)"
+        let alert=UIAlertController (title: "Hello,world!", message: message, preferredStyle: .Alert)
+        let action=UIAlertAction (title: "OK", style: .Default, handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(slider:UISlider){
-        print("The value of the slider is now: \(slider.value)")
-        currentSliderValue=(slider.value)
+        //print("The value of the slider is now: \(slider.value)")
+        currentSliderValue=lroundf(slider.value)
+    }
+    
+    func startNewRound(){
+        targetValue=1+Int(arc4random_uniform(100))
+        currentSliderValue=50;
+        slider.value=Float(currentSliderValue)
     }
 
 
