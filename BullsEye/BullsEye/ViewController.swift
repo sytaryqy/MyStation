d5a418ab62a174668320c7acf5f6a0f33724a5be
sytar <sytar@sytartekimac.local>
2016-01-11 16:53:35 +0800

    BullsEye.Finished all the logic of the game.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentSliderValue:Int=0;
+//    var currentSliderValue:Int=0
+//    var targetValue:Int=0
     @IBOutlet weak var slider: UISlider!
-    var targetValue:Int=0;
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel:UILabel!
+    
+    //using type inference
+    var currentSliderValue=0
+    var targetValue=0
+    var score=0
+    var rounds = -1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +28,7 @@ class ViewController: UIViewController {
         //currentSliderValue=lroundf(slider.value)
         //targetValue=1+Int(arc4random_uniform(100))
         startNewRound()
+        updateLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,12 +37,40 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert(){
-        let message="The value of the slider is: \(currentSliderValue)"+"\nThe target value is: \(targetValue)"
-        let alert=UIAlertController (title: "Hello,world!", message: message, preferredStyle: .Alert)
+        
+//        var difference:Int
+//        
+//        if currentSliderValue > targetValue{
+//            difference = currentSliderValue - targetValue
+//        }else if targetValue > currentSliderValue{
+//            difference = targetValue - currentSliderValue
+//        }else {
+//            difference = 0
+//        }
+        let difference=abs(targetValue - currentSliderValue)
+        let points=100-difference
+//        let message="Your score:\(points)"
+        score += points
+        var message = "Your score:\(points)"
+        var playerMessage = " "
+        if points == 100 {
+            playerMessage="Perfect!"
+            message = message + "\nYou get another 100 points!"
+            score += 100
+        }else if points >= 80{
+            playerMessage = "You almost had it!"
+        }else if points >= 60{
+            playerMessage = "Not bad!"
+        }else {
+            playerMessage = "Not even close!"
+        }
+//        let message="The value of the slider is: \(currentSliderValue)"+"\nThe target value is: \(targetValue)"+"\nThe difference value is:\(difference)"
+        let alert=UIAlertController (title: playerMessage, message: message, preferredStyle: .Alert)
         let action=UIAlertAction (title: "OK", style: .Default, handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
         startNewRound()
+        updateLabel()
     }
     
     @IBAction func sliderMoved(slider:UISlider){
@@ -44,6 +82,13 @@ class ViewController: UIViewController {
         targetValue=1+Int(arc4random_uniform(100))
         currentSliderValue=50;
         slider.value=Float(currentSliderValue)
+        rounds += 1
+    }
+    
+    func updateLabel(){
+        targetLabel.text=String(targetValue)
+        scoreLabel.text=String(score)
+        roundLabel.text=String(rounds)
     }
 
 
