8ff2be08b683fd0c7716f1094973b6bbf4a5f852
sytar <sytar@sytartekimac.local>
2016-02-02 15:42:25 +0800

    BullsEye-Finished the makeover.Now I begin edit for iPhone4S.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -24,6 +24,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set the slider
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable =
+            trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable =
+            trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
         //currentSliderValue=lroundf(slider.value)
         //targetValue=1+Int(arc4random_uniform(100))
