f0ff955690828ba14bf9ada1784ad3d0023b2f07
sytar <sytar@sytartekimac.local>
2016-02-06 11:06:45 +0800

    Checklist--Reuse the cells

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -20,6 +20,29 @@ class ChecklistViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return 100
+    }
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("ChecklistItem")! as UITableViewCell
+            
+            let label = cell.viewWithTag(1000) as! UILabel
+            if indexPath.row % 5 == 0 {
+                label.text = "Walk the dog"
+            } else if indexPath.row % 5 == 1 {
+                label.text = "Brush my teeth"
+            } else if indexPath.row % 5 == 2 {
+                label.text = "Learn iOS development"
+            } else if indexPath.row % 5 == 3 {
+                label.text = "Soccer practice"
+            } else if indexPath.row % 5 == 4 {
+                label.text = "Eat ice cream"
+            }
+            return cell
+    }
 
 }
 
