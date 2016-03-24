a3f6f5c9ba1109605fc63232650d24a2f6b07047
sytar <sytar@sytartekimac.local>
2016-03-24 17:05:53 +0800

    First use variables to save the datas

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    var row0text = "Walk the dog"
+    var row1text = "Brush teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Scorre practice"
+    var row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +34,108 @@ class ChecklistViewController: UITableViewController {
 
     override func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
-            return 100
+            return 5
     }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cell=tableView.cellForRowAtIndexPath(indexPath){
+            /*
+            if cell.accessoryType == .None{
+                cell.accessoryType = .Checkmark
+            }else{
+                cell.accessoryType = .None
+            }
+            */
+            /*
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+                if (row0checked){
+                    cell.accessoryType = .Checkmark
+                }else{
+                    cell.accessoryType = .None
+                }
+            }
+            if indexPath.row == 1 {
+                row1checked = !row1checked
+                if (row1checked){
+                    cell.accessoryType = .Checkmark
+                }else{
+                    cell.accessoryType = .None
+                }
+            }
+            if indexPath.row == 2 {
+                row2checked = !row2checked
+                if (row2checked){
+                    cell.accessoryType = .Checkmark
+                }else{
+                    cell.accessoryType = .None
+                }
+            }
+            if indexPath.row == 3 {
+                row3checked = !row3checked
+                if (row3checked){
+                    cell.accessoryType = .Checkmark
+                }else{
+                    cell.accessoryType = .None
+                }
+            }
+            if indexPath.row == 4 {
+                row4checked = !row4checked
+                if (row4checked){
+                    cell.accessoryType = .Checkmark
+                }else{
+                    cell.accessoryType = .None
+                }
+            }
+            */
+            
+            var isChecked = false
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+                isChecked = row0checked
+                }
+            if indexPath.row == 1 {
+                row1checked = !row1checked
+                isChecked = row1checked
+            }
+            if indexPath.row == 2 {
+                row2checked = !row2checked
+                isChecked = row2checked
+            }
+            if indexPath.row == 3 {
+                row3checked = !row3checked
+                isChecked = row3checked
+            }
+            if indexPath.row == 4 {
+                row4checked = !row4checked
+                isChecked = row4checked
+            }
+            if isChecked{
+                cell.accessoryType = .Checkmark
+            }else{
+                cell.accessoryType = .None
+            }
+            
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+    }
+    
     override func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
             let cell =
             tableView.dequeueReusableCellWithIdentifier("ChecklistItem")! as UITableViewCell
             
             let label = cell.viewWithTag(1000) as! UILabel
-            if indexPath.row % 5 == 0 {
-                label.text = "Walk the dog"
-            } else if indexPath.row % 5 == 1 {
-                label.text = "Brush my teeth"
-            } else if indexPath.row % 5 == 2 {
-                label.text = "Learn iOS development"
-            } else if indexPath.row % 5 == 3 {
-                label.text = "Soccer practice"
-            } else if indexPath.row % 5 == 4 {
-                label.text = "Eat ice cream"
+            if indexPath.row  == 0 {
+                label.text = row0text
+            } else if indexPath.row  == 1 {
+                label.text = row1text
+            } else if indexPath.row  == 2 {
+                label.text = row2text
+            } else if indexPath.row  == 3 {
+                label.text = row3text
+            } else if indexPath.row  == 4 {
+                label.text = row4text
             }
             return cell
     }
