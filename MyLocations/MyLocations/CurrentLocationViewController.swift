28cfbbc044cc1ad1dc8d07aff705949ce2a558dc
sytar <sytar@sytartekimac.local>
2016-04-27 16:24:41 +0800

    Try to get the location

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -7,9 +7,40 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
 
+class CurrentLocationViewController: UIViewController,CLLocationManagerDelegate {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    @IBAction func getLocation() {
+        
+        let authStatus: CLAuthorizationStatus =
+        CLLocationManager.authorizationStatus()
+        
+        if authStatus == .NotDetermined{
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .Denied || authStatus == .Restricted{
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+    }
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +49,33 @@ class CurrentLocationViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: - showLocationServicesDeniedAlert
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled",
+            message:
+            "Please enable location services for this app in Settings.",
+            preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default,
+            handler: nil)
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(locationManager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]){
+            if let newLocation = locations.last {
+                print("didUpdateLocations \(newLocation)")
+            }
+    }
+    
+    func locationManager(locationManager: CLLocationManager,
+        didFailWithError error: NSError){
+            print("didFailWithError \(error)")
     }
 
 
